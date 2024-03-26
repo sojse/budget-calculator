f2496f0ff95c2a27e7c8fff725b17df272cbadb7
Josefine Isberg <95719327+sojse@users.noreply.github.com>
2024-03-26 16:11:45 +0100

    Create budget typeDefs

---
+++ src/graphql/schema/budget/types.ts
@@ -0,0 +1,31 @@
+export const BudgetTypes = `
+scalar DateTime
+
+type Budget {
+	id: ID!
+	title: String!
+	description: String
+	startDate: DateTime!
+	endDate: DateTime!
+	createdAt: DateTime
+	updatedAt: DateTime
+	expenses: [Expense]!
+	income: [Income]!
+}
+
+type Query {
+	budget(id: ID!): Budget
+	budgets: [Budget]
+}
+
+input BudgetCreateDataInput {
+	title: String!
+	description: String
+	startDate: DateTime!
+	endDate: DateTime!
+}
+
+type Mutation {
+	budgetCreate(data: BudgetCreateDataInput!): Budget
+}
+`;
