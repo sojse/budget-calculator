81de3651ebb991907085ceebdde75842968a774d
Josefine Isberg <95719327+sojse@users.noreply.github.com>
2024-03-26 16:11:27 +0100

    Add query to get all budgets

--- src/graphql/schema/budget/query.ts
+++ src/graphql/schema/budget/query.ts
@@ -1,6 +1,9 @@
 import prisma from "@/graphql/db";
 
 export const BudgetQuery = {
+  budgets: () => {
+    return prisma.budget.findMany({});
+  },
   budget: (_: any, args: { id: string }) => {
     return prisma.budget.findFirst({
       where: { id: args.id },
