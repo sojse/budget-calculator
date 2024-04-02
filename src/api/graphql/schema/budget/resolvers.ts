23105433acd4d47dd1e98aeb6b2845c96c8d5b70
Josefine Isberg <95719327+sojse@users.noreply.github.com>
2024-04-02 13:50:45 +0200

    Add resolver to get related expenses to a budget

--- src/api/graphql/schema/budget/resolvers.ts
+++ src/api/graphql/schema/budget/resolvers.ts
@@ -1,35 +1,42 @@
-/**
- // product/resolvers.js
-export const ProductResolvers = {
-    // here we only write a resolver for reviews and apollo server will create a default 
-    // resolver for other fields.
-    reviews: (parent, args) => {
-        // whatever
-    }
-}
- */
-
 import prisma from '@/api/prisma/db';
 
-// TODO - add resolver when expenses and incomes are added
 export const BudgetResolvers = {
-  incomes: async (parent: any) => {
-    try {
-      // Retrieve incomes associated with the budget
-      const incomes = await prisma.income.findMany({
-        where: {
-          budgets: {
-            some: {
-              id: parent.id,
-            },
-          },
-        },
-      });
-      return incomes;
-    } catch (error) {
-      throw new Error(
-        `Failed to fetch incomes for budget with ID: ${parent.id}`,
-      );
-    }
-  },
+	incomes: async (parent: any) => {
+		try {
+			// Retrieve incomes associated with the budget
+			const incomes = await prisma.income.findMany({
+				where: {
+					budgets: {
+						some: {
+							id: parent.id,
+						},
+					},
+				},
+			});
+			return incomes;
+		} catch (error) {
+			throw new Error(
+				`Failed to fetch incomes for budget with ID: ${parent.id}`
+			);
+		}
+	},
+	expenses: async (parent: any) => {
+		try {
+			// Retrieve expenses associated with the budget
+			const expense = await prisma.expense.findMany({
+				where: {
+					budgets: {
+						some: {
+							id: parent.id,
+						},
+					},
+				},
+			});
+			return expense;
+		} catch (error) {
+			throw new Error(
+				`Failed to fetch incomes for budget with ID: ${parent.id}`
+			);
+		}
+	},
 };
