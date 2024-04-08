6286b4eaa8af65f8f07050ea2f4a5e762515d3c3
Josefine Isberg <95719327+sojse@users.noreply.github.com>
2024-04-08 15:43:24 +0200

    Create form label component

---
+++ src/ui/components/1-atoms/Form/Label/Label.stories.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { StoryFn, Meta } from '@storybook/react';
+import { Label, LabelProps } from './Label';
+
+export default {
+	title: 'Atoms/Form/Label',
+	component: Label,
+	tags: ['autodocs'],
+} as Meta;
+
+const Template: StoryFn<LabelProps> = (args) => <Label {...args} />;
+
+export const Default: StoryFn<LabelProps> = Template.bind({});
+Default.args = {
+	id: 'label',
+	children: 'Label',
+};
+
+export const Required: StoryFn<LabelProps> = Template.bind({});
+Required.args = {
+	...Default.args,
+	state: {
+		required: true,
+	},
+};
+
+export const Disabled: StoryFn<LabelProps> = Template.bind({});
+Disabled.args = {
+	...Default.args,
+	state: {
+		disabled: true,
+	},
+};
+
+export const Error: StoryFn<LabelProps> = Template.bind({});
+Error.args = {
+	...Default.args,
+	state: {
+		hasError: true,
+	},
+};
+
+export const Hidden: StoryFn<LabelProps> = Template.bind({});
+Hidden.args = {
+	...Default.args,
+	state: {
+		isHidden: true,
+	},
+};
---
+++ src/ui/components/1-atoms/Form/Label/Label.tsx
@@ -0,0 +1,36 @@
+import classNames from 'classnames';
+import styles from './Label.module.scss';
+
+export type LabelProps = {
+	className?: string;
+	id: string;
+	state?: {
+		required?: boolean;
+		disabled?: boolean;
+		hasError?: boolean;
+		isHidden?: boolean;
+	};
+	children?: React.ReactNode;
+};
+
+export const Label: React.FC<LabelProps> = ({
+	className,
+	id,
+	children,
+	state,
+}) => (
+	<label
+		className={classNames(
+			styles.label,
+			{ [styles.hasError]: state?.hasError },
+			{ [styles.isDisabled]: state?.disabled },
+			{ [styles.isHidden]: state?.isHidden },
+			{ [styles.isRequired]: state?.required },
+			className
+		)}
+		htmlFor={id}
+	>
+		<span>{children}</span>
+		{state?.required && <span aria-hidden="true">*</span>}
+	</label>
+);
