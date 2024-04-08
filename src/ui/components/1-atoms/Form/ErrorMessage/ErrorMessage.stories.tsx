4882730ade13ed525436dd60f338989d71643bd0
Josefine Isberg <95719327+sojse@users.noreply.github.com>
2024-04-08 15:43:37 +0200

    Create error message component

---
+++ src/ui/components/1-atoms/Form/ErrorMessage/ErrorMessage.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StoryFn, Meta } from '@storybook/react';
+import { ErrorMessage, ErrorMessageProps } from './ErrorMessage';
+
+export default {
+	title: 'Atoms/Form/ErrorMessage',
+	component: ErrorMessage,
+	tags: ['autodocs'],
+} as Meta;
+
+const Template: StoryFn<ErrorMessageProps> = (args) => (
+	<ErrorMessage {...args} />
+);
+
+export const Default: StoryFn<ErrorMessageProps> = Template.bind({});
+Default.args = {
+	id: 'error',
+	children: 'An error has occured!',
+};
---
+++ src/ui/components/1-atoms/Form/ErrorMessage/ErrorMessage.tsx
@@ -0,0 +1,18 @@
+import classNames from 'classnames';
+import styles from './ErrorMessage.module.scss';
+
+export type ErrorMessageProps = {
+	className?: string;
+	id: string;
+	children?: React.ReactNode;
+};
+
+export const ErrorMessage: React.FC<ErrorMessageProps> = ({
+	className,
+	id,
+	children,
+}) => (
+	<div id={id} className={classNames(styles.error_message, className)}>
+		{children}
+	</div>
+);
