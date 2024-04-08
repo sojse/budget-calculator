ac45361b186c05765b5a3795df7c434b3b6bedea
Josefine Isberg <95719327+sojse@users.noreply.github.com>
2024-04-08 14:47:36 +0200

    Configure sass and storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,21 +1,39 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import path from 'path';
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
-  ],
-  framework: {
-    name: "@storybook/nextjs",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
-  staticDirs: ["..\\public"],
+	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+	addons: [
+		'@storybook/addon-onboarding',
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@chromatic-com/storybook',
+		'@storybook/addon-interactions',
+		'@storybook/addon-styling-webpack',
+	],
+	framework: {
+		name: '@storybook/nextjs',
+		options: {},
+	},
+	docs: {
+		autodocs: 'tag',
+	},
+	staticDirs: ['..\\public'],
+	webpackFinal: async (config) => {
+		if (!config.resolve) {
+			config.resolve = {};
+		}
+
+		if (!config.resolve.alias) {
+			config.resolve.alias = {};
+		}
+
+		config.resolve.alias['@'] = path.resolve(__dirname, '../src');
+
+		config.module = config.module || {};
+		config.module.rules = config.module.rules || [];
+
+		return config;
+	},
 };
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from "@storybook/react";
+import '../src/ui/styles/globals.scss';
 
 const preview: Preview = {
   parameters: {
