f595dd4125f64faae8fff0cf8ce120e76b0f557f
Josefine Isberg <95719327+sojse@users.noreply.github.com>
2024-04-09 14:38:37 +0200

    Install SVGR package

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -35,6 +35,21 @@ const config: StorybookConfig = {
 		config.module = config.module || {};
 		config.module.rules = config.module.rules || [];
 
+		// This modifies the existing image rule to exclude .svg files
+		// to enable the use of svgr/webpack
+		const imageRule = config.module.rules.find((rule) =>
+			rule?.['test']?.test('.svg')
+		);
+		if (imageRule) {
+			imageRule['exclude'] = /\.svg$/;
+		}
+
+		// Configure .svg files to be loaded with @svgr/webpack
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: ['@svgr/webpack'],
+		});
+
 		return config;
 	},
 };
--- next.config.js
+++ next.config.js
@@ -5,5 +5,15 @@ module.exports = {
 		includePaths: [path.join(__dirname, 'styles')],
 		prependData: `@import '@/ui/styles/variables.scss';`,
 	},
+
+	webpack: (config) => {
+		// Add SVGR loader to enable using SVGs as React Components
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: [{ loader: '@svgr/webpack', options: { icon: true } }],
+		});
+
+		return config;
+	},
 };
 
