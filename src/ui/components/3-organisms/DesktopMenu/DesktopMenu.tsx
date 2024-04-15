cc618543ad6e215000cd0ad04c25b366776bdad0
Josefine Isberg <95719327+sojse@users.noreply.github.com>
2024-04-15 15:02:23 +0200

    Move desktop menu to own component

---
+++ src/ui/components/3-organisms/DesktopMenu/DesktopMenu.tsx
@@ -0,0 +1,32 @@
+'use client';
+import classNames from 'classnames';
+import styles from './DesktopMenu.module.scss';
+import { LinkComponent } from '@/ui/components';
+
+export interface DesktopMenuProps {
+	navigation: {
+		url: string;
+		label: string;
+		isActive: boolean;
+	}[];
+	className?: string;
+}
+
+export const DesktopMenu = ({ navigation, className }: DesktopMenuProps) => {
+	return (
+		<nav className={classNames(styles.desktop_navigation, className)}>
+			{navigation.map((item: any, index: any) => (
+				<LinkComponent
+					key={index}
+					url={item.url}
+					style="dark"
+					className={classNames(
+						item.isActive && styles.desktop_navigation_link__active
+					)}
+				>
+					{item.label}
+				</LinkComponent>
+			))}
+		</nav>
+	);
+};
