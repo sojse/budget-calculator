4cf12bd07df1a971928dd9c8cb0040e35d913029
Josefine Isberg <95719327+sojse@users.noreply.github.com>
2024-04-19 13:49:13 +0200

    Create year helper

--- src/helpers/date.tsx
+++ src/helpers/date.tsx
@@ -2,3 +2,9 @@ export const isValidDate = (dateString: string) => {
 	const date = new Date(dateString);
 	return !isNaN(date.getTime());
 };
+
+export const extractYear = (dateString: string) => {
+	const date = new Date(dateString);
+	const year = date.getFullYear().toString();
+	return year;
+};
