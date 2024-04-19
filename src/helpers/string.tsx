d237aa41ea06a3a35265f827fd33fc81ee1492db
Josefine Isberg <95719327+sojse@users.noreply.github.com>
2024-04-19 13:48:56 +0200

    Add string helpers

--- src/helpers/string.tsx
+++ src/helpers/string.tsx
@@ -1,2 +1,12 @@
 export const capitalizeFirstLetter = (string: string): string =>
 	string.charAt(0).toUpperCase() + string.slice(1);
+
+
+
+export const buildNavigationString = (string1: string, string2: string) => {
+	return `${string1}/${string2}`;
+};
+
+export const buildBudgetTitle = (string1: string, string2: string) => {
+	return `${capitalizeFirstLetter(string1)} ${string2}`;
+};
