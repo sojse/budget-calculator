eb844d25da891dd51b0144dd6abb5254e3ea463a
Josefine Isberg <95719327+sojse@users.noreply.github.com>
2024-04-25 14:16:17 +0200

    Create dynamic form in modal

--- src/app/@modal/(.)modal/[id]/page.tsx
+++ src/app/@modal/(.)modal/[id]/page.tsx
@@ -1,9 +1,14 @@
-import { CreateBudgetForm, Modal } from '@/ui/components';
+import { capitalizeFirstLetter } from '@/helpers/string';
+import { Modal } from '@/ui/components';
+import * as Forms from '@/ui/components/3-organisms/Forms';
 
-export default function ModalPage({ params }: { params: { slug: string } }) {
+export default function ModalPage({ params }: { params: { id: string } }) {
+	const formName = `${capitalizeFirstLetter(params.id)}Form`;
+	const DynamicForm = Forms[formName as keyof typeof Forms];
+
 	return (
 		<Modal>
-			<CreateBudgetForm />
+			<DynamicForm />
 		</Modal>
 	);
 }
