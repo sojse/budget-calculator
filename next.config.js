21291dfeb7960fcc4e8b2ed3d4227cac8a125c75
Josefine Isberg <95719327+sojse@users.noreply.github.com>
2024-05-13 11:31:14 +0200

    Add ppr configuration

--- next.config.js
+++ next.config.js
@@ -1,24 +1,25 @@
 const path = require('path');
 
-module.exports = {
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	experimental: {
+		ppr: true,
+	},
 	sassOptions: {
 		includePaths: [path.join(__dirname, 'styles')],
 		prependData: `@import '@/ui/styles/variables.scss';`,
 	},
-
-	nextConfig: {
-		experimental: {
-			ppr: true,
-		},
-	},
-
 	webpack: (config) => {
 		// Add SVGR loader to enable using SVGs as React Components
 		config.module.rules.push({
 			test: /\.svg$/,
 			use: [{ loader: '@svgr/webpack', options: { icon: true } }],
 		});
+
 		return config;
 	},
 };
+
+module.exports = nextConfig;
 
