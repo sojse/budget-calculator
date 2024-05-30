13a23440dcc93f4425eb7f650461740c5ebf04cc
Josefine Isberg <95719327+sojse@users.noreply.github.com>
2024-05-30 13:23:21 +0200

    Adjust endpoint

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,4 +1,5 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { registerApolloClient } from '@apollo/experimental-nextjs-app-support/rsc';
 //import { onError } from '@apollo/link-error';
 //import { createUploadLink } from '@apollo-upload-client';
 //import '../dotenv.config';
@@ -77,11 +78,10 @@ export const getClient = () => {
 	if (!client || typeof window === 'undefined') {
 		client = new ApolloClient({
 			link: new HttpLink({
-				uri: 'http://localhost:4000/graphql',
+				uri: 'http://localhost:8000/graphql',
 			}),
 			cache: new InMemoryCache(),
 		});
 	}
 	return client;
 };
-
